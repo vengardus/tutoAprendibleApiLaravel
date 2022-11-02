6b1ab0f45258c8a7fde6f4e9732ab85ae530e763
ismytv <ismytv@gmail.com>
2022-11-02 18:52:35 -0500

    first class - 01

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,14 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BookApiTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
+    
+    function can_get_all_books() {
 
-        $response->assertStatus(200);
     }
 }
